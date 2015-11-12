322696e5fdb90f2d046f1a88cb152ba9d39e741d
16348104 <16348104@QQ.com>
2015-11-13 00:57:08 +0800

    update er

--- src/exercise/Son.java
+++ src/exercise/Son.java
@@ -4,18 +4,12 @@ public class Son extends Father {
        public void m1(){
 //    	   super.m();
        }
-
-	/* （非 Javadoc）
-	 * @see exercise.Father#m()
-	 */
 	public void m() {
-		// TODO 自动生成的方法存根
 		super.m();
 		System.out.println("Son...");
 	}
-
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+
          Son s=new Son();
          s.m1();
          s.m();
