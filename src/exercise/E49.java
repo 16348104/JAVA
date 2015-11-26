a9d44fbd51e2345f9adf640e056d98ba0fcd8c12
16348104 <16348104@QQ.com>
2015-11-26 23:06:43 +0800

    update e2849

--- src/exercise/E49.java
+++ src/exercise/E49.java
@@ -1,11 +1,26 @@
-//计算字符串中子串出现的次数。
 package exercise;
 
+//计算字符串中子串出现的次数。
 public class E49 {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-
+		String str1 = new String("lenovo");
+		String str2 = new String("o");
+		int len=str1.length();
+		int count=0;
+        for(int i=0;i<len;i++){
+        	int j=str1.indexOf(str2);
+        	if(j==-1)
+        	{
+        		continue;
+        	}
+        	else{
+        		count++;
+        	}
+        	String s=str1.substring(j+1, len);
+        }
+        System.out.println(count);
+        }
 	}
 
-}
+
