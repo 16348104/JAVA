aefc75c61733868a723ffe786ddb6740b7304c02
16348104 <16348104@QQ.com>
2015-11-27 22:53:07 +0800

    update  E49

--- src/exercise/E49.java
+++ src/exercise/E49.java
@@ -4,23 +4,21 @@ package exercise;
 public class E49 {
 
 	public static void main(String[] args) {
-		String str1 = new String("lenovo");
-		String str2 = new String("o");
-		int len=str1.length();
-		int count=0;
-        for(int i=0;i<len;i++){
-        	int j=str1.indexOf(str2);
-        	if(j==-1)
-        	{
-        		continue;
-        	}
-        	else{
-        		count++;
-        	}
-        	String s=str1.substring(j+1, len);
-        }
-        System.out.println(count);
-        }
+		String s1 = "lenovo";
+		String s2 = "o";
+		int len1=s1.length();
+		int len2 = s2.length();
+		int count = 0;
+		int start=0;
+		for (int i = 0; i <len1 ; i++) {
+			int size = s1.indexOf(s2,start);
+            start=size+len2;
+            if(size>0&&start<len1)
+            {
+            	count++;
+            }
+		}
+
+		System.out.println("字符串"+s1+"中"+"子串"+s2+"出现的次数为"+count);
 	}
-
-
+}
