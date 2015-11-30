5efe8d4945ffb75e0ddf0184f7eb8c69e5f84eba
16348104 <16348104@QQ.com>
2015-11-30 21:58:51 +0800

    update

--- src/exercise/E37.java
+++ src/exercise/E37.java
@@ -1,10 +1,36 @@
 package exercise;
 //有n个人围成一圈，顺序排号。从第一个人开始报数（从1到3报数），凡报到3的人退出圈子，问最后留下的是原来第几号的那位。
+import java.util.Scanner;
+
 public class E37 {
-
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		boolean[] arr = new boolean[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = true;// 下标为TRUE时说明还在圈里
+		}
+		int leftCount = n;
+		int countNum = 0;
+		int index = 0;
+		while (leftCount > 1) {
+			if (arr[index] == true) {// 当在圈里时
+				countNum++; // 报数递加
+				if (countNum == 3) {// 报道3时
+					countNum = 0;// 从零开始继续报数
+					arr[index] = false;// 此人退出圈子
+					leftCount--;// 剩余人数减一
+				}
+			}
+			index++;// 每报一次数，下标加一
+			if (index == n) {// 是循环数数，当下标大于n时，说明已经数了一圈，
+				index = 0;// 将下标设为零重新开始。
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == true) {
+				System.out.println(i);
+			}
+		}
 	}
-
 }
--- src/exercise/E49.java
+++ src/exercise/E49.java
@@ -6,19 +6,18 @@ public class E49 {
 	public static void main(String[] args) {
 		String s1 = "lenovo";
 		String s2 = "o";
-		int len1=s1.length();
+		int len1 = s1.length();
 		int len2 = s2.length();
 		int count = 0;
-		int start=0;
-		for (int i = 0; i <len1 ; i++) {
-			int size = s1.indexOf(s2,start);
-            start=size+len2;
-            if(size>0&&start<len1)
-            {
-            	count++;
-            }
+		int start = 0;
+		for (int i = 0; i < len1; i++) {
+			int size = s1.indexOf(s2, start);
+			start = size + len2;
+			if (size > 0 && start < len1) {
+				count++;
+			}
 		}
 
-		System.out.println("字符串"+s1+"中"+"子串"+s2+"出现的次数为"+count);
+		System.out.println("字符串" + s1 + "中" + "子串" + s2 + "出现的次数为" + count);
 	}
 }
