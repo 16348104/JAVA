1764443193feb2a61e60c4f4a652e9b707f4495c
16348104 <16348104@QQ.com>
2016-01-09 00:48:28 +0800

    test

--- src/exercise/E13.java
+++ src/exercise/E13.java
@@ -4,5 +4,9 @@ package exercise;
  * Created by dell on 2015/10/28.
  * 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
  */
-public class E13 {
+
+public  class E13 {
+	private String a;
+	private Integer b;
+
 }
---
+++ src/exercise/test.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class test {
+
+	public static void main(String[] args) {
+		String  s="hello";
+		String t =s+"good";
+//		t=s[3]+"one";
+		System.out.print('2'+'2');
+//		String temp ={"a","b","c"};
+		String temp []={"av","bjjj","ccb"};
+		char ch[]={'a','c'};
+
+	}
+
+}
